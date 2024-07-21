0d46e27149522e779040cf9c20ef8f497e6bcc65
Frozen Controller <frozenco@ez-p6.mag.21-school.ru>
2024-07-21 14:39:47 +0300

    fix memset

--- src/s21_memset.c
+++ src/s21_memset.c
@@ -1,12 +1,13 @@
 #include "s21_string.h"
-
+ 
 void *s21_memset(void *str, int c, s21_size_t n) {
-    unsigned char *stroke = (unsigned char *)str;
+    unsigned char *stroke = str;
+    unsigned char v = (unsigned char )c;
 
     for (s21_size_t i = 0; i < n; i++) {
-        stroke[i] = (char)c;
+        stroke[i] = v;
     }
-    
+
     return str;
 }
 
